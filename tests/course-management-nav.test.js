9eef2d9b7bf0837cae1e2059261d6c6e5443562b
carrie <carrieing@outlook.com>
2026-04-12 10:46:01 +0800

    refactor: split course management pages

---
+++ tests/course-management-nav.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+
+const html = fs.readFileSync(path.join(__dirname, '../public/index.html'), 'utf8');
+
+assert.match(html, /<div class="sb-sec">教学管理<\/div>/, 'sidebar should group teaching pages');
+assert.match(html, /<div class="sb-sec">课程管理<\/div>/, 'sidebar should group course pages');
+assert.match(html, /<div class="sb-sec">场地与资源<\/div>/, 'sidebar should group resource pages');
+
+assert.match(html, /goPage\('products',this\)[\s\S]*?课程产品/, 'sidebar should keep products page');
+assert.match(html, /goPage\('packages',this\)[\s\S]*?售卖课包/, 'sidebar should add packages page');
+assert.match(html, /goPage\('purchases',this\)[\s\S]*?购买记录/, 'sidebar should add purchases page');
+assert.match(html, /goPage\('entitlements',this\)[\s\S]*?权益账户/, 'sidebar should add entitlements page');
+
+assert.match(html, /id="page-packages"/, 'should have packages page section');
+assert.match(html, /id="page-purchases"/, 'should have purchases page section');
+assert.match(html, /id="page-entitlements"/, 'should have entitlements page section');
+
+console.log('course management nav tests passed');
